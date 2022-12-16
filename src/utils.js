3024a2c2c589a0affabd811c82352053a81e8adc
RuslanPro01 <ruslan91999@gmail.com>
2022-12-17 02:58:25 +0300

    Реализует функцию подсчета длительности времени

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,14 @@
 import dayjs from 'dayjs';
 import {
   DateFormat,
-  MAX_OFFER_PRICE
+  Minutes,
+  TimeUnits
 } from './const.js';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import duration from 'dayjs/plugin/duration';
+
+dayjs.extend(relativeTime);
+dayjs.extend(duration);
 
 const getRandomPrice = (maxPrice) => Math.floor(Math.random() * maxPrice / 100) * 100;
 const getRandomNumber = (number) => Math.round(Math.random() * number);
@@ -14,7 +20,7 @@ const RandomPointDate = {
     return dateFrom.toISOString();
   },
   GET_DATE_TO() {
-    return dateFrom.add(getRandomNumber(MAX_OFFER_PRICE), 'day').add(getRandomNumber(1000), 'minute').toISOString();
+    return dateFrom.add(getRandomNumber(3), 'day').add(getRandomNumber(1000), 'minute').toISOString();
   }
 };
 
@@ -22,6 +28,22 @@ const makeCapitalizeFirstLetter = (string) => string.charAt(0).toLowerCase() + s
 const getRandomArrayElement = (array) => array[getRandomNumber(array.length - 1)];
 const getRandomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 const getFormatDate = (date, format = DateFormat.HUMANIZE) => date ? dayjs(date).format(format) : '';
+const getDifferenceConventionalUnits = (dayTo, dayForm, units) => dayjs(dayTo).diff(dayjs(dayForm), units);
+
+const getHumanizeDiffTime = (dayTo, dayForm) => {
+  const diffMinutes = getDifferenceConventionalUnits(dayTo, dayForm, TimeUnits.MINUTE);
+  const durationTime = dayjs.duration(diffMinutes, 'minutes');
+
+  if (diffMinutes < Minutes.IN_HOUR) {
+    return durationTime.format(DateFormat.SHORT_TIME);
+  }
+
+  if (diffMinutes >= Minutes.IN_HOUR && diffMinutes < Minutes.IN_DAY) {
+    return durationTime.format(DateFormat.AVERAGE_TIME);
+  }
+
+  return durationTime.format(DateFormat.LURGE_TIME);
+};
 
 export {
   makeCapitalizeFirstLetter,
@@ -30,5 +52,6 @@ export {
   RandomPointDate,
   getRandomNumber,
   getRandomIntFromInterval,
+  getHumanizeDiffTime,
   getFormatDate
 };
