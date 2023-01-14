ddc36084ba1c97bda4e2df9fe1d019b620a5e222
RuslanPro01 <ruslan91999@gmail.com>
2023-01-14 18:10:59 +0300

    Позволит скрывать блоки описания и офферов, если нет данных по ним

--- src/const.js
+++ src/const.js
@@ -1,7 +1,7 @@
 const MAX_PRICE = 10000;
 const NUMBER_POINTS_CREATED = 10;
 const POINT_TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
-const CITY_DESCRIPTIONS = ['lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor', 'sit amet luctus venenatis lectus magna fringilla urna porttitor rhoncus', 'dictum sit amet justo donec enim diam vulputate ut pharetra', 'ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper', 'dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo', 'elementum curabitur vitae nunc sed velit dignissim sodales ut eu', 'feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit'];
+const CITY_DESCRIPTIONS = ['', 'lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor', 'sit amet luctus venenatis lectus magna fringilla urna porttitor rhoncus', 'dictum sit amet justo donec enim diam vulputate ut pharetra', '', 'ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper', 'dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo', 'elementum curabitur vitae nunc sed velit dignissim sodales ut eu', 'feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit', ''];
 const CITIES = ['Toronto', 'Quebec', 'Vancouver', 'Calgary', 'Ottawa', 'Montreal', 'Victoria', 'St. John\'s', 'Charlottetown', 'Saskatoon', 'Whitehorse', 'Halifax'];
 
 const DateFormat = {
--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -126,7 +126,7 @@ function createEditPointTemplate(point, destinations, allOffers) {
           </button>
         </header>
         <section class="event__details">
-          <section class="event__section  event__section--offers">
+          <section class="event__section  event__section--offers" ${!offersByType ? 'style="display: none"' : ''}>
             <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
             <div class="event__available-offers">
@@ -134,7 +134,7 @@ function createEditPointTemplate(point, destinations, allOffers) {
             </div>
           </section>
 
-          <section class="event__section  event__section--destination">
+          <section class="event__section  event__section--destination" ${!description ? 'style="display: none"' : ''}>
             <h3 class="event__section-title  event__section-title--destination">Destination</h3>
             <p class="event__destination-description">${description}</p>
           </section>
